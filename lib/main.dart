8f6d6fda2756af66d5ea629bca5f9b5b996fbc99
MuhammedU05 <uamuhammed05@gmail.com>
2023-11-07 19:57:25 +0530

    .

--- lib/main.dart
+++ lib/main.dart
@@ -50,33 +50,3 @@ class MyApp extends StatelessWidget {
     );
   }
 }
-
-class LoggedIn extends StatelessWidget {
-  const LoggedIn({super.key});
-
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return ScreenUtilInit(
-      designSize: const Size(375.0, 812.0),
-      minTextAdapt: true,
-      splitScreenMode: true,
-      useInheritedMediaQuery: true,
-      builder: (BuildContext context, child) {
-        return MaterialApp(
-          title: KStrings.appName,
-          debugShowCheckedModeBanner: false,
-          initialRoute: RouteNames.splashScreen,
-          onGenerateRoute: RouteNames.generateRoutes,
-          builder: (context, child) {
-            return MediaQuery(
-              data: MediaQuery.of(context).copyWith(textScaleFactor: 1.0),
-              child: child!,
-            );
-          },
-          theme: MyTheme.theme,
-        );
-      },
-    );
-  }
-}
